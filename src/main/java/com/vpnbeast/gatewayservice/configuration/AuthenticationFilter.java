98012c6051fb646adc110aaa6c2c91c43618db01
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-05-30 21:30:04 +0300

    return json response on filter

--- src/main/java/com/vpnbeast/gatewayservice/configuration/AuthenticationFilter.java
+++ src/main/java/com/vpnbeast/gatewayservice/configuration/AuthenticationFilter.java
@@ -1,18 +1,25 @@
 package com.vpnbeast.gatewayservice.configuration;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vpnbeast.gatewayservice.model.ExceptionInfo;
 import com.vpnbeast.gatewayservice.service.JwtService;
+import com.vpnbeast.gatewayservice.util.DateUtil;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
 import java.util.ArrayList;
 
 @Slf4j
@@ -23,6 +30,7 @@ public class AuthenticationFilter implements GatewayFilter {
 
     private final RouterValidator routerValidator;
     private final JwtService jwtService;
+    private final ObjectMapper objectMapper;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -32,12 +40,13 @@ public class AuthenticationFilter implements GatewayFilter {
                 return onError(exchange, "Authorization header is missing in request", HttpStatus.UNAUTHORIZED);
 
             final String token = getAuthHeader(request).substring(7);
-            log.info("token = {}", token);
 
             if (!jwtService.isTokenValid(token)) {
                 log.info("token is invalid");
                 return onError(exchange, "Authorization header is invalid", HttpStatus.UNAUTHORIZED);
             }
+
+            // TODO: check if token is expired
 
             populateRequestWithHeaders(exchange, token, jwtService.getUsernameFromToken(token));
         }
@@ -46,9 +55,21 @@ public class AuthenticationFilter implements GatewayFilter {
     }
 
     private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
-        ServerHttpResponse response = exchange.getResponse();
-        response.setStatusCode(httpStatus);
-        return response.setComplete();
+        try {
+            ServerHttpResponse response = exchange.getResponse();
+            DataBuffer db = response.bufferFactory().wrap(objectMapper.writeValueAsBytes(ExceptionInfo.builder()
+                    .httpCode(httpStatus.value())
+                    .status(false)
+                    .errorMessage(err)
+                    .timestamp(DateUtil.getCurrentLocalDateTime())
+                    .build()));
+            response.setStatusCode(httpStatus);
+            response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+            return response.writeWith(Mono.just(db));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return Mono.empty();
+        }
     }
 
     private String getAuthHeader(ServerHttpRequest request) {
---
+++ src/main/java/com/vpnbeast/gatewayservice/model/ExceptionInfo.java
@@ -0,0 +1,20 @@
+package com.vpnbeast.gatewayservice.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+public class ExceptionInfo {
+
+    private final String tag;
+    private final String errorMessage;
+    private final Boolean status;
+    private final Integer httpCode;
+    private final LocalDateTime timestamp;
+
+}
