d406beb4d813452c24e11ed06d47540b3962ca39
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-06-02 00:05:04 +0300

    add todo [ci-skip]

--- src/test/java/com/vpnbeast/gatewayservice/GatewayServiceApplicationTests.java
+++ src/test/java/com/vpnbeast/gatewayservice/GatewayServiceApplicationTests.java
@@ -3,6 +3,7 @@ package com.vpnbeast.gatewayservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// TODO: better way to handle test properties
 @SpringBootTest(properties = {
         "spring.cloud.config.enabled=false",
         "gateway-service.client.auth-service.listOfServers=http://localhost:5000",
