d98b46403422fcb81961b01b51782d3ae3bc9226
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-06-02 00:04:34 +0300

    fix test error

--- src/test/java/com/vpnbeast/gatewayservice/GatewayServiceApplicationTests.java
+++ src/test/java/com/vpnbeast/gatewayservice/GatewayServiceApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
         "spring.cloud.config.enabled=false",
+        "gateway-service.client.auth-service.listOfServers=http://localhost:5000",
         "gateway-service.upstream.vpnbeastServiceUrl=http://localhost:9090",
         "gateway-service.upstream.authServiceUrl=http://localhost:5000"})
 class GatewayServiceApplicationTests {
