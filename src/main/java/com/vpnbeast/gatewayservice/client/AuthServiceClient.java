a20399b1fb08fbbbb5ff2813a37daea90ce019a0
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-06-06 01:50:55 +0300

    remove unneccessary auth-service request

--- src/main/java/com/vpnbeast/gatewayservice/client/AuthServiceClient.java
+++ src/main/java/com/vpnbeast/gatewayservice/client/AuthServiceClient.java
@@ -1,7 +1,5 @@
 package com.vpnbeast.gatewayservice.client;
 
-import com.vpnbeast.gatewayservice.model.UserRequest;
-import com.vpnbeast.gatewayservice.model.UserResponse;
 import com.vpnbeast.gatewayservice.model.ValidateTokenRequest;
 import com.vpnbeast.gatewayservice.model.ValidateTokenResponse;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -10,9 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "auth-service", url = "${gateway-service.client.auth-service.url}")
 public interface AuthServiceClient {
-
-    @PostMapping(value = "/auth/user")
-    UserResponse isValidUser(@RequestBody UserRequest userRequest);
 
     @PostMapping(value = "/auth/validate")
     ValidateTokenResponse validateToken(@RequestBody ValidateTokenRequest validateTokenRequest);
