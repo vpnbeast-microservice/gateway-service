265be259d1d135d1ed3f0aa93b5766c63e6171ad
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-06-17 00:44:02 +0300

    proper handling of test properties

--- src/test/java/com/vpnbeast/gatewayservice/GatewayServiceApplicationTests.java
+++ src/test/java/com/vpnbeast/gatewayservice/GatewayServiceApplicationTests.java
@@ -3,14 +3,8 @@ package com.vpnbeast.gatewayservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// TODO: better way to handle test properties
-@SpringBootTest(properties = {
-        "spring.cloud.config.enabled=false",
-        "gateway-service.client.auth-service.url=http://localhost:5000",
-        "gateway-service.upstream.vpnbeast-service.url=http://localhost:9090",
-        "gateway-service.upstream.vpnbeast-service.uris='/users/**,/servers/**,/admin/**'",
-        "gateway-service.upstream.auth-service.url=http://localhost:5000",
-        "gateway-service.upstream.auth-service.uris='/auth/**'"})
+// @ActiveProfiles("unit-test")
+@SpringBootTest(properties = {"spring.cloud.config.enabled=false"})
 class GatewayServiceApplicationTests {
 
     @Test
