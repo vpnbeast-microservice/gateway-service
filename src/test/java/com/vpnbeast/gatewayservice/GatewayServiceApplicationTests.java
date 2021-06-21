f7779e27d2f5cf5909bd8bab03edf9da19aa96be
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-06-21 23:19:01 +0300

    pass config server enabled as bootstrap property

--- src/test/java/com/vpnbeast/gatewayservice/GatewayServiceApplicationTests.java
+++ src/test/java/com/vpnbeast/gatewayservice/GatewayServiceApplicationTests.java
@@ -3,8 +3,7 @@ package com.vpnbeast.gatewayservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @ActiveProfiles("unit-test")
-@SpringBootTest(properties = {"spring.cloud.config.enabled=false"})
+@SpringBootTest
 class GatewayServiceApplicationTests {
 
     @Test
