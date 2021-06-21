0dc1fcbc122191a73263886e5d972f2c243d2634
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-06-21 23:24:10 +0300

    disable config server by annotation

--- src/test/java/com/vpnbeast/gatewayservice/GatewayServiceApplicationTests.java
+++ src/test/java/com/vpnbeast/gatewayservice/GatewayServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.vpnbeast.gatewayservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.cloud.config.enabled=false"})
 class GatewayServiceApplicationTests {
 
     @Test
